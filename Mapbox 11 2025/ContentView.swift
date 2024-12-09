69504e82955297dfa3daa84e1b0a641ab2b47710
Jim Margolis <57914060+JimMargolis@users.noreply.github.com>
2024-12-09 15:55:01 -0700

    Initial Commit

---
+++ Mapbox 11 2025/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mapbox 11 2025
+//
+//  Created by Jim Margolis on 12/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mapbox 11 2025/Mapbox_11_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  Mapbox_11_2025App.swift
+//  Mapbox 11 2025
+//
+//  Created by Jim Margolis on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Mapbox_11_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
